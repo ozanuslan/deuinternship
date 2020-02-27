9b9192766ccb39d27230474f8286bf6718750a70
ozanuslan <ozanuslan@hotmail.com>
2020-02-27 19:37:26 +0300

    openfile added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import java.io.FileNotFoundException;
+
 public class Main {
+    static Company[] COMPANIES;
+    static Department[] DEPARTMENTS;
+    static Internship[] INTERNSHIPS;
 
-    Company[] COMPANIES;
-    Department[] DEPARTMENTS;
-    Internship[] INTERNSHIPS;
-
-    public static void main(String[] args) {
-
-    }
+    public static void main(String[] args) throws FileNotFoundException {
+        Control ctl = new Control();
+        ctl.openFile();
+    }    
 }
